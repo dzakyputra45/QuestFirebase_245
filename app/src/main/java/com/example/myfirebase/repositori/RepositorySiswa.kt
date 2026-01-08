5c1ce356a3e861b4d0ff4fa943dfa7e461a7b485
dzakyputra45 <dzakypratama45@gmail.com>
2026-01-08 21:04:50 +0700

    Update pada RepositorySiswa.kt untuk getsatusiswa edit dan hapus

--- app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
@@ -7,6 +7,9 @@ import kotlinx.coroutines.tasks.await
 interface RepositorySiswa {
     suspend fun getDataSiswa(): List<Siswa>
     suspend fun postDataSiswa(siswa: Siswa)
+    suspend fun getSatuSiswa(id: String): Siswa?
+    suspend fun editSatuSiswa(id: String, siswa: Siswa)
+    suspend fun hapusSatuSiswa(id: String)
 }
 
 class FirebaseRepositorySiswa : RepositorySiswa {
@@ -18,30 +21,59 @@ class FirebaseRepositorySiswa : RepositorySiswa {
         return try {
             collection.get().await().documents.map { doc ->
                 Siswa(
-                    id = doc.getLong("id")?.toLong() ?: 0,
+                    id = doc.id,
                     nama = doc.getString("nama") ?: "",
                     alamat = doc.getString("alamat") ?: "",
                     telpon = doc.getString("telpon") ?: ""
                 )
+
             }
         } catch (e: Exception) {
             emptyList()
         }
     }
 
-    override suspend fun postDataSiswa(siswa: Siswa) {
-        val docRef = if (siswa.id == 0L)
-            collection.document()
-        else
-            collection.document(siswa.id.toString())
 
+    override suspend fun postDataSiswa(siswa: Siswa) {
         val data = hashMapOf(
-            "id" to (siswa.id.takeIf { it != 0L } ?: docRef.id.hashCode()),
             "nama" to siswa.nama,
             "alamat" to siswa.alamat,
             "telpon" to siswa.telpon
         )
 
-        docRef.set(data).await()
+        collection.add(data).await()
+    }
+
+    override suspend fun getSatuSiswa(id: String): Siswa? {
+        return try {
+            val doc = collection.document(id).get().await()
+            if (!doc.exists()) return null
+
+            Siswa(
+                id = doc.id,
+                nama = doc.getString("nama") ?: "",
+                alamat = doc.getString("alamat") ?: "",
+                telpon = doc.getString("telpon") ?: ""
+            )
+        } catch (e: Exception) {
+            println("Gagal Baca Data siswa: ${e.message}")
+            null
+        }
+    }
+
+
+    override suspend fun editSatuSiswa(id: String, siswa: Siswa) {
+        collection.document(id).set(
+            mapOf(
+                "nama" to siswa.nama,
+                "alamat" to siswa.alamat,
+                "telpon" to siswa.telpon
+            )
+        ).await()
+    }
+
+
+    override suspend fun hapusSatuSiswa(id: String) {
+        collection.document(id).delete().await()
     }
 }
